237c0af53af35c2be8b30b7a40e38085d572e3b0
KhaledHemdan <136645522+KhaledHemdan@users.noreply.github.com>
2024-01-04 21:13:32 +0200

    Updated

---
+++ 102-binary_tree_is_complete.c
@@ -0,0 +1,130 @@
+#include "binary_trees.h"
+
+levelorder_queue_t *create_node(binary_tree_t *node);
+void free_queue(levelorder_queue_t *head);
+void push(binary_tree_t *node, levelorder_queue_t *head,
+		levelorder_queue_t **tail);
+void pop(levelorder_queue_t **head);
+int binary_tree_is_complete(const binary_tree_t *tree);
+
+/**
+ * create_node - The Definition I've to write to pass.
+ * @node: The Definition I've to write to pass.
+ *
+ * Return: The Definition I've to write to pass.
+ */
+levelorder_queue_t *create_node(binary_tree_t *node)
+{
+	levelorder_queue_t *new;
+
+	new = malloc(sizeof(levelorder_queue_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->node = node;
+	new->next = NULL;
+
+	return (new);
+}
+
+/**
+ * binary_tree_is_complete - The Definition I've to write to pass.
+ * @tree: The Definition I've to write to pass.
+ *
+ * Return: The Definition I've to write to pass.
+ *
+ * Description: The Definition I've to write to pass.
+ */
+int binary_tree_is_complete(const binary_tree_t *tree)
+{
+	levelorder_queue_t *head, *tail;
+	unsigned char flag = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	head = tail = create_node((binary_tree_t *)tree);
+	if (head == NULL)
+		exit(1);
+
+	while (head != NULL)
+	{
+		if (head->node->left != NULL)
+		{
+			if (flag == 1)
+			{
+				free_queue(head);
+				return (0);
+			}
+			push(head->node->left, head, &tail);
+		}
+		else
+			flag = 1;
+		if (head->node->right != NULL)
+		{
+			if (flag == 1)
+			{
+				free_queue(head);
+				return (0);
+			}
+			push(head->node->right, head, &tail);
+		}
+		else
+			flag = 1;
+		pop(&head);
+	}
+	return (1);
+}
+
+/**
+ * push - The Definition I've to write to pass.
+ * @node: The Definition I've to write to pass.
+ * @head: The Definition I've to write to pass.
+ * @tail: The Definition I've to write to pass.
+ *
+ * Description: The Definition I've to write to pass.
+ */
+void push(binary_tree_t *node, levelorder_queue_t *head,
+		levelorder_queue_t **tail)
+{
+	levelorder_queue_t *new;
+
+	new = create_node(node);
+	if (new == NULL)
+	{
+		free_queue(head);
+		exit(1);
+	}
+	(*tail)->next = new;
+	*tail = new;
+}
+
+/**
+ * pop - The Definition I've to write to pass.
+ * @head: The Definition I've to write to pass.
+ */
+void pop(levelorder_queue_t **head)
+{
+	levelorder_queue_t *tmp;
+
+	tmp = (*head)->next;
+	free(*head);
+	*head = tmp;
+}
+
+
+/**
+ * free_queue - The Definition I've to write to pass.
+ * @head: The Definition I've to write to pass.
+ */
+void free_queue(levelorder_queue_t *head)
+{
+	levelorder_queue_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
