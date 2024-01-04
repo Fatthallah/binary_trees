9e0ddf10ef8daa3c9e24916f65186c275c9aaab8
KhaledHemdan <136645522+KhaledHemdan@users.noreply.github.com>
2024-01-04 21:21:08 +0200

    Updated

---
+++ 134-heap_to_sorted_array.c
@@ -0,0 +1,54 @@
+#include "binary_trees.h"
+
+
+
+/**
+ * heap_to_sorted_array - The Definition I've to write to pass.
+ *
+ * @heap: The Definition I've to write to pass.
+ * @size: The Definition I've to write to pass.
+ *
+ * Return: The Definition I've to write to pass.
+ **/
+int *heap_to_sorted_array(heap_t *heap, size_t *size)
+{
+	int i, *a = NULL;
+
+	if (!heap || !size)
+		return (NULL);
+
+	*size = tree_size(heap) + 1;
+
+	a = malloc(sizeof(int) * (*size));
+
+	if (!a)
+		return (NULL);
+
+	for (i = 0; heap; i++)
+		a[i] = heap_extract(&heap);
+
+	return (a);
+}
+
+/**
+ * tree_size - The Definition I've to write to pass.
+ * @tree: The Definition I've to write to pass.
+ *
+ * Return: The Definition I've to write to pass.
+ */
+size_t tree_size(const binary_tree_t *tree)
+{
+	size_t height_l = 0;
+	size_t height_r = 0;
+
+	if (!tree)
+		return (0);
+
+	if (tree->left)
+		height_l = 1 + tree_size(tree->left);
+
+	if (tree->right)
+		height_r = 1 + tree_size(tree->right);
+
+	return (height_l + height_r);
+}
